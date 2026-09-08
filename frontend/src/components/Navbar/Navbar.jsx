380234a8a68acdf2b7d0b0fe537a68d8c19e8da1
princethakur19 <princethakur7778@gmail.com>
2026-09-08 20:41:59 +0530

    Refactor Navbar component to manage menu state and enhance cart display

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop")
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -10,14 +14,15 @@ const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop <hr/></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop {menu === "shop" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("men")}}>Men {menu === "men" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women {menu === "women" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids {menu === "kids" ? <hr/> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
