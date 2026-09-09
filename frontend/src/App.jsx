7411bbea0480143b6f7d5cb7bac89345807a3de2
princethakur19 <princethakur7778@gmail.com>
2026-09-09 11:49:02 +0530

    Add routing and new pages for Shop, ShopCategory, Product, Cart, and LoginSignup; update index.html for font integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import ShopCategory from './pages/ShopCategory'
+import Shop from './pages/Shop'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/men' element={<ShopCategory/>}/>
+          <Route path='/women' element={<ShopCategory/>}/>
+          <Route path='/kids' element={<ShopCategory/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
