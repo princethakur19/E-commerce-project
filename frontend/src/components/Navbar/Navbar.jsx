b9efdcb528e3e6f0e8ae02962f5da3bb8a09d1f9
princethakur19 <princethakur7778@gmail.com>
2026-09-09 17:03:19 +0530

    Refactor Navbar styles and structure; add Hero component with styling

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -15,14 +15,14 @@ const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link> {menu === "shop" ? <hr/> : <></>}</li>
-        <li onClick={()=>{setMenu("men")}}><Link to='/men'>Men</Link> {menu === "men" ? <hr/> : <></>}</li>
-        <li onClick={()=>{setMenu("women")}}><Link to='/women'>Women</Link> {menu === "women" ? <hr/> : <></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kids</Link> {menu === "kids" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none', color: '#686868'}} to='/'>Shop</Link> {menu === "shop" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration: 'none', color: '#686868'}} to='/men'>Men</Link> {menu === "men" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration: 'none', color: '#686868'}} to='/women'>Women</Link> {menu === "women" ? <hr/> : <></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none', color: '#686868'}} to='/kids'>Kids</Link> {menu === "kids" ? <hr/> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
-        <img src={cart_icon} alt="" />
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
