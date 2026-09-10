773ead6d37fe8cf441adc55938f51bc01b4d1f91
princethakur19 <princethakur7778@gmail.com>
2026-09-10 17:19:42 +0530

    Add Popular content releated to the women section. Passing props through Items into Popular and Mount in Shop.

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/hero/Hero'
+import Popular from '../components/Popular/Popular'
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   )
 }
