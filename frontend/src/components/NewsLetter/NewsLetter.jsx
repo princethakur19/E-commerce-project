e97fb40f71cd390ba8273a83d599c600769ffc61
princethakur19 <princethakur7778@gmail.com>
2026-09-11 16:32:03 +0530

    Add NewsLetter component and styles for email subscription

---
+++ frontend/src/components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './NewsLetter.css'
+
+const NewsLetter = () => {
+  return (
+    <div className="newsletter">
+
+      <h1>Get Exclusive Offers To Your Email</h1>
+
+      <p>
+        Subscribe to our newsletter and stay updated
+      </p>
+
+      <div className="newsletter-form">
+        <input
+          type="email"
+          placeholder="Your Email ID"
+        />
+
+        <button>
+          Subscribe
+        </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default NewsLetter
