2b4e495f0c84d38fb2d1ce1e57d4e54e455a6912
princethakur19 <princethakur7778@gmail.com>
2026-09-16 16:03:49 +0530

    Add styling to ShopCategory component and apply class to banner image

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext)
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -22,6 +22,9 @@ const ShopCategory = (props) => {
         {all_product.map((item, i)=>{
           if(props.category === item.category){
             return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          }
+          else{
+            return null;
           }
         })}
       </div>
